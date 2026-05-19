6571022d9e19e864eb30be82a88cd2b239bd5c73
Isaac Violeth <isaacviolet1234@gmail.com>
2026-05-19 15:50:37 -0500

    feat(frontend): add angular initial setup and service connection test

---
+++ frontend/paideia-frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { TestConnectionComponent } from './shared/components/test-connection/test-connection';
+
+export const routes: Routes = [
+  { path: '', component: TestConnectionComponent }
+];
---
+++ frontend/paideia-frontend/src/app/shared/components/test-connection/test-connection.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestConnection } from './test-connection';
+
+describe('TestConnection', () => {
+  let component: TestConnection;
+  let fixture: ComponentFixture<TestConnection>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TestConnection],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestConnection);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
